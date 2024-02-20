498edb2764ea7bd69369476bce7e239164310d17
Gabriel Cabrejas <gabriel@cabrejas.net>
2024-02-20 19:26:06 +0100

    wip

--- frontend/index.js
+++ frontend/index.js
@@ -2,11 +2,15 @@ import './styles/reset.css'
 import './styles/styles.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './state/store'
 import App from './components/App'
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
 
 root.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,6 +1,14 @@
-export const resetStore = () => {
-  let store = {}
-  return store
+import { configureStore } from '@reduxjs/toolkit'
+
+const exampleReducer = (state = { count: 0 }) => {
+  return state
 }
+
+export const resetStore = () => configureStore({
+  reducer: {
+    example: exampleReducer,
+    // add your own reducers here
+  },
+})
 
 export const store = resetStore()
